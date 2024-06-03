5a91d0a575ff860397db7a87936cc870808f31f0
Andrew Sweet <andrewmsweet@gmail.com>
2024-06-03 08:48:00 -0400

    Update DAG description

--- src/app/experience/dbtLabs.tsx
+++ src/app/experience/dbtLabs.tsx
@@ -24,7 +24,7 @@ export const dbtLabs = {
     TypeScript, React, Rust, Node.js, Python, Postgres, Redis, and GraphQL.
 
     - Transformed my hackathon-winning project into dbt Explorer, a flag-ship product for dbt Labs.
-    - Built and maintained multiple npm packages, including dbt-dag (a React library for visualizing dbt resources across dbt Cloud) and dagnabbit (a Rust library for handling selector syntax parsing and graph traversal problems).`.trim(),
+    - Built and maintained multiple npm packages, including dbt-dag (a React library for visualizing dbt resources across dbt Cloud as a Directed Acyclic Graph) and dagnabbit (a Rust library for handling selector syntax parsing and graph traversal for subgraph selection).`.trim(),
 
     externalWebsite: 'https://www.getdbt.com/',
     heroImageSrc: '/dag.png',
