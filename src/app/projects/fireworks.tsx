3af882e2f6178344f252be4dad9f20c0ec5f82a7
Andrew Sweet <andrewmsweet@gmail.com>
2024-06-08 12:44:06 -0400

    Update text of fireworks demo

--- src/app/projects/fireworks.tsx
+++ src/app/projects/fireworks.tsx
@@ -18,18 +18,20 @@ export const fireworks = {
     startDate: null,
     endDate: null,
     description: <div className="space-y-4">
-    <div>C++, OpenFrameworks, 3D grid of LEDs</div>
+    <div>C++, OpenFrameworks, Octrees, 3D grid of LEDs</div>
 
     <div>
     A 3D grid of LEDs suspended in space light up as a
-    3D particle simulation replicating fireworks intersects them.
-
-    This simulation takes advantage of QuadTrees to efficiently
+    3D particle simulation of fireworks collides with them.
+    This simulation takes advantage of Octrees to efficiently
     identify which LEDs should be lit up to make a firework effect.
-
+    </div>
+    <div>
     Made for a temporary installation.
     </div>
-    <div className="mt-4"><a href="https://www.youtube.com/watch?v=_JalqOQanLE" target="_blank">See video</a></div>
+    <div className="mt-4">
+        <a href="https://www.youtube.com/watch?v=_JalqOQanLE" target="_blank">See virtual fireworks demo</a>
+    </div>
     </div>,
     href: "/projects#fireworks",
     preview: <Preview />,
