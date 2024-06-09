65c014a5f30e0b2aca1639dc0d77e0bdcdca47db
Andrew Sweet <andrewmsweet@gmail.com>
2024-06-09 15:18:01 -0400

    Update page layout for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,9 +43,10 @@ const technologies = [
 
 export default function LandingPage() {
   return (
-    <>
+    <div className='md:mx-16 md:my-16'>
       <div className='flex flex-col md:flex-row'>
-        <span className='flex-0'>
+        <div className='flex-0 flex flex-col md:flex-row'>
+          <div className='my-auto flex-0 mr-4'>
             <Image
                 className='skip-chroma animate-img-load'
                 src="/256.png"
@@ -53,12 +54,12 @@ export default function LandingPage() {
                 priority 
                 height={256}
                 width={256} />
-        </span>
-        <span className='ml-4 flex-1'>
-          <h1 className='text-5xl whitespace-nowrap'>Andrew Sweet</h1>
-          <div className='text-2xl mt-2'>
+          </div>
+          <div className='flex-1 my-auto'>
+            <h1 className='text-5xl whitespace-nowrap'>Andrew Sweet</h1>
+            <div className='text-2xl mt-2'>
+              <h2 className='sr-only'>External Links</h2>
               <div className='space-x-2 mb-4'>
-                <h2 className='sr-only'>External Links</h2>
                 <a href='https://www.linkedin.com/in/andrewmsweet/' target='_blank'>
                   <Image role='presentation' alt='' src='/linkedin.png' className='inline-block mb-1 mr-1' width={16} height={16} />
                   LinkedIn
@@ -83,52 +84,55 @@ export default function LandingPage() {
                   </Marquee>
                 </div>
               </div>
-              <div className='text-green my-4 space-y-2'>
-                <h2 className='sr-only'>About Me</h2>
-                <div>
-                I enjoy solving hard technical problems, and I have a passion
-                for creating polished, accessible, and performant user
-                experiences.
-                
-                I have a degree
-                in <a href='/education#cmu'>Computer Science and Art from Carnegie Mellon University</a>,
-                and am always looking to solve creative technical challenges.
-                
-                I have a strong background in web apps and
-                full-stack development, and even had the opportunity to build
-                a <a target='_blank' href='https://iontank.com/projects/drumbar'>
-                  <Image src='/iontank.png' className='inline-block mb-1 mr-1' alt='Iontank' width={16} height={16} />
-                  5-story cylindrical TV screen
-                </a>.
-                </div>
-                <div>
-                I am currently working as a Senior Software Engineer
-                at <a href='/experience#dbt-labs'>dbt Labs</a>, where I
-                turned my hackathon win into a flagship product: <a target='_blank' href="https://www.getdbt.com/blog/proactively-improve-your-dbt-projects-with-new-dbt-explorer-features">
-                  <Image src='/dbt.png' className='inline-block mb-1 mr-1' alt='dbt Labs' width={16} height={16} />
-                  dbt Explorer
-                </a>.
-                
-                Formerly, I worked
-                at companies like <a href='/experience#google-shopping'>Google</a>,
-                {' '}<a href='/experience#nike'>Nike</a>, and
-                {' '}<a href='/experience#iontank'>Iontank</a>.
-                </div>
-                <div>
-                In my free time, I enjoy making videogames -- most recently having created
-                {' '}<a href='https://itch.io/jam/miz-jam-1/rate/738711' className='whitespace-break-spaces' target='_blank'>
-                  <Image role='presentation' alt='' src='/bridgei.png' className='inline-block mb-1 mr-1' width={16} height={16} />
-                  Bridge: The Card Game: The Video Game
-                </a> for a game jam (
-                  <a href="https://www.youtube.com/watch?v=lj866DtYVlo" target='_blank'>
-                    <Image src='/youtube.png' className='inline-block mb-1 mr-1' alt='YouTube' width={16} height={16} />
-                    Dev Vlog
-                  </a>).
-                </div>
-              </div>
+            </div>
           </div>
-        </span>
+        </div>
       </div>
-    </>
+      <div className='flex-1'>
+        <div className='text-green my-4 space-y-2 text-2xl'>
+          <h2 className='sr-only'>About Me</h2>
+          <div>
+          I enjoy solving hard technical problems, and I have a passion
+          for creating polished, accessible, and performant user
+          experiences.
+          
+          I have a degree
+          in <a href='/education#cmu'>Computer Science and Art from Carnegie Mellon University</a>,
+          and am always looking to solve creative technical challenges.
+          
+          I have a strong background in web apps and
+          full-stack development, and even had the opportunity to build
+          a <a target='_blank' href='https://iontank.com/projects/drumbar'>
+            <Image src='/iontank.png' className='inline-block mb-1 mr-1' alt='Iontank' width={16} height={16} />
+            5-story cylindrical TV screen
+          </a>.
+          </div>
+          <div>
+          I am currently working as a Senior Software Engineer
+          at <a href='/experience#dbt-labs'>dbt Labs</a>, where I
+          turned my hackathon win into a flagship product: <a target='_blank' href="https://www.getdbt.com/blog/proactively-improve-your-dbt-projects-with-new-dbt-explorer-features">
+            <Image src='/dbt.png' className='inline-block mb-1 mr-1' alt='dbt Labs' width={16} height={16} />
+            dbt Explorer
+          </a>.
+          
+          Formerly, I worked
+          at companies like <a href='/experience#google-shopping'>Google</a>,
+          {' '}<a href='/experience#nike'>Nike</a>, and
+          {' '}<a href='/experience#iontank'>Iontank</a>.
+          </div>
+          <div>
+          In my free time, I enjoy making videogames -- most recently having created
+          {' '}<a href='https://itch.io/jam/miz-jam-1/rate/738711' className='whitespace-break-spaces' target='_blank'>
+            <Image role='presentation' alt='' src='/bridgei.png' className='inline-block mb-1 mr-1' width={16} height={16} />
+            Bridge: The Card Game: The Video Game
+          </a> for a game jam (
+            <a href="https://www.youtube.com/watch?v=lj866DtYVlo" target='_blank'>
+              <Image src='/youtube.png' className='inline-block mb-1 mr-1' alt='YouTube' width={16} height={16} />
+              Dev Vlog
+            </a>).
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
